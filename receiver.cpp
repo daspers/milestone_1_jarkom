e208f7315ec26861500e54b23d60cbfea4eebb94
Tony <buddy90_lost@yahoo.co.id>
2018-10-31 23:13:21 +0700

    update sender and receiver

--- receiver.cpp
+++ receiver.cpp
@@ -6,14 +6,106 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <algorithm>
 
-#define BUFSIZE 1024
+#define MAX_DATA_LENGTH 1024
+#define ACK_NUMBER 0x6
+#define NAK_NUMBER 0x21
+
+struct frame
+{
+	int seq_num;
+	int length;
+	uint8_t data[1024];
+	frame(){
+		seq_num = 0;
+		length = 0;
+	}
+};
+
+class FileWriter
+{
+public:
+	FileWriter(char *filename, uint32_t buffer_size) : max_buffer_size(buffer_size){
+		writer = fopen(filename, "wb");
+		buffer = (uint8_t *) malloc(max_buffer_size);
+		offset = 0;
+	}
+	~FileWriter(){
+		flush();
+		fclose(writer);
+	}
+	void write_data(uint8_t *data, uint32_t length){
+		if(length == 0)
+			return;
+		if(offset == max_buffer_size)
+			flush();
+		uint32_t len = std::min(max_buffer_size - offset, length);
+		memcpy(buffer+offset, data, len);
+		offset += len;
+		write_data(data+len, length - len);
+	}
+private:
+	const uint32_t max_buffer_size;
+	FILE *writer;
+	uint8_t *buffer;
+	uint32_t offset;
+	void flush(){
+		fwrite(buffer, sizeof(uint8_t), offset, writer);
+		offset = 0;
+	}
+};
+
+uint8_t checksum(const uint8_t *data, uint32_t len){
+	/* Not Finish */
+	return 0;
+}
+
+bool verify_packet(const uint8_t *data, uint32_t len){
+	if(data[0] != 0x1)
+		return false;
+	if(checksum(data, len-1) != data[len-1])
+		return false;
+	return true;
+}
+
+uint32_t bytes_to_int(uint8_t *data){
+	uint32_t res = 0;
+	for(int i=0;i<4;++i){
+		res <<= 8;
+		res += data[i];
+	}
+	return res;
+}
+
+void fill_int_to_bytes(uint8_t *data, uint32_t val){
+	for(int i=3;i>=0;--i){
+		data[i] = val & 0xFFFF;
+		val >>= 8;
+	}
+}
+
+void send_resp(int head, int n_seq_num, int sd, sockaddr_in &my_addr){
+	printf("Send ack %d : ", n_seq_num);
+	uint8_t ack[6];
+	ack[0] = head;
+	fill_int_to_bytes(ack+1, n_seq_num);
+	ack[5] = checksum(ack, 5);
+	printf("%d %d %d %d %d %d\n", (int)ack[0], (int)ack[1], (int)ack[2], (int)ack[3], (int)ack[4], (int)ack[5]);
+	if(sendto(sd, ack, 6, 0, (struct sockaddr *) &my_addr, sizeof my_addr) < 0)
+		perror("sendto ack");
+}
 
 int main(int argc, char **argv){
-	char *port = argv[1];
+	char *filename = argv[1];
+	int window_size = atoi(argv[2]);
+	int buff_size = atoi(argv[3]); 
+	char *port = argv[4];
+	FileWriter writer(filename, buff_size*MAX_DATA_LENGTH);
+
 	int sd = socket(AF_INET, SOCK_DGRAM, 0);
 	if(sd < 0){
-		perror("sock_init error");
+		perror("sock_init");
 		exit(1);
 	}
 	struct sockaddr_in my_addr;
@@ -30,20 +122,52 @@ int main(int argc, char **argv){
 		perror("bind failed");
 		exit(1);
 	}
+	
+	/* Timeout time for program to wait for packet */
+	struct timeval tv;
+	tv.tv_sec = 2;
+	tv.tv_usec = 0;
+	setsockopt(sd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof tv); 
 
-	char data[102];
+	/* Start receiving data */
+	uint8_t packet[1035];
+	frame *data = (frame *) malloc(window_size * sizeof(frame));
+	uint32_t lfr = 0, laf = 10;
 	while(1){
-		int recvlen = recvfrom(sd, data, 100, 0, (struct sockaddr *) &my_addr, &addrlen);
-		if(recvlen > 0 && recvlen <= 100){
-			data[recvlen] = '\0';
-			printf("%s\n", data);
-		}
-		else{
-			perror("receive failed");
-			close(sd);
-			exit(1);
+
+		/* Read Incoming packet */
+		int recvlen = recvfrom(sd, packet, MAX_DATA_LENGTH+10, 0, (struct sockaddr *) &my_addr, &addrlen);
+		printf("receive %d %d\n", recvlen, (int)packet[0]);
+		if(recvlen > 0 && recvlen <= MAX_DATA_LENGTH+10){
+			if(verify_packet(packet, recvlen)){
+				printf("Packet size : %d\n", recvlen);
+				int seq_num = bytes_to_int(packet+1);
+				printf("Packet receive no : %d\n", seq_num);
+				if(seq_num > lfr && seq_num <= laf){
+					int idx = seq_num % window_size;
+					if(data[idx].seq_num != seq_num){
+						data[idx].seq_num = seq_num;
+						data[idx].length = bytes_to_int(packet+5);
+						if(data[idx].length==0) break;
+						memcpy(data[idx].data, packet+9, data[idx].length);
+						
+						/* Move Window */
+						while(data[(lfr+1)%window_size].seq_num == lfr+1){
+							writer.write_data(data[idx].data, data[idx].length);
+							++lfr;
+						}
+						laf = lfr + window_size;
+					}
+					
+					/* Send ACK */
+					send_resp(ACK_NUMBER, lfr+1, sd, my_addr);
+				}
+			}
+			else
+				send_resp(NAK_NUMBER, lfr+1, sd, my_addr);
 		}
 	}
-	close(sd);
+
+	// close(sd);
 	return 0;
 }
--- sender.cpp
+++ sender.cpp
@@ -6,29 +6,221 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <queue>
+#include <ctime>
+#include <algorithm>
+
+#define MAX_DATA_LENGTH 1024
+#define timeout_limit 1
+#define ACK_NUMBER 0x6
+#define NAK_NUMBER 0x21
+
+struct pack_timeout
+{
+	int packet_no;
+	time_t timeout;
+	pack_timeout(){
+		packet_no = 0;
+		timeout = 0;
+	}
+	pack_timeout(int _packet_no, time_t _timeout){
+		packet_no = _packet_no;
+		timeout = _timeout;
+	}
+};
+
+struct frame
+{
+	uint32_t length;
+	uint8_t data[1024];
+	time_t timeout;
+};
+
+class FileReader
+{
+public:
+	size_t size;
+	size_t rem_size;
+	FileReader(char *filename, uint32_t buffer_size) : max_buffer_size(buffer_size){
+		struct stat st;
+		reader = fopen(filename, "rb");
+		buffer = (uint8_t *) malloc(max_buffer_size);
+		fill_buffer();
+		stat(filename, &st);
+		rem_size = size = st.st_size;
+	}
+	~FileReader(){
+		fclose(reader);
+	}
+	void read_data(uint8_t *data, uint32_t length){
+		if(length == 0)
+			return;
+		if(offset == current_size)
+			fill_buffer();
+		if(current_size - offset < length){
+			int len = std::min(length, current_size - offset);
+			memcpy(data, buffer + offset, len);
+			offset += len;
+			rem_size -= len;
+			read_data(data+len, length-len);
+		}
+		else{
+			memcpy(data, buffer + offset, length);
+			offset += length;
+			rem_size -= length;
+		}
+	}
+private:
+	FILE *reader;
+	const uint32_t max_buffer_size;
+	uint32_t current_size;
+	uint32_t offset;
+	uint8_t *buffer;
+	void fill_buffer(){
+		offset = 0;
+		current_size = fread(buffer, sizeof(uint8_t), max_buffer_size, reader);
+	}
+};
+
+void fill_int_to_bytes(uint8_t *data, uint32_t val){
+	for(int i=3;i>=0;--i){
+		data[i] = val & 0xFFFF;
+		val >>= 8;
+	}
+}
+
+uint8_t checksum(const uint8_t *data, uint32_t len){
+	return 0;
+}
+
+uint32_t bytes_to_int(uint8_t *data){
+	uint32_t res = 0;
+	for(int i=0;i<4;++i){
+		res <<= 8;
+		res += data[i];
+	}
+	return res;
+}
+
+bool verify_data(const uint8_t *data, uint32_t len){
+	if(data[0] != 0x1)
+		return false;
+	if(checksum(data, len-1) != data[len-1])
+		return false;
+	return true;
+}
+
+bool verify_response(const uint8_t *data, uint32_t len){
+	return checksum(data, len-1) == data[len-1];
+}
+
+void sent_packet(int seq_num, uint8_t *data, int data_length, int sd, struct sockaddr_in &in_name){
+	uint8_t packet[MAX_DATA_LENGTH + 10];
+	packet[0] = 0x1;
+	fill_int_to_bytes(packet+1, seq_num);
+	fill_int_to_bytes(packet+5, data_length);
+	memcpy(packet+9, data, data_length);
+	// checksum begin not ready :(
+	packet[data_length+9] = checksum(packet, data_length+9);
+	// checksum end
+	printf("Send packet NO: %d\n", seq_num);
+	if(sendto(sd, packet, (size_t) (data_length + 10), 0, (struct sockaddr *) &in_name, sizeof in_name) < 0)
+		perror("send");
+}
 
 int main(int argc, char **argv){
-	char *host = argv[1];
-	char *port = argv[2];
+	if(argc != 6){
+		puts("Error! wrong format");
+		exit(1);
+	}
+	char *filename = argv[1];
+	int window_size = atoi(argv[2]);
+	int buff_size = atoi(argv[3]); 
+	char *host = argv[4];
+	char *port = argv[5];
 	int sd = socket(AF_INET, SOCK_DGRAM, 0);
+	FileReader reader(filename, buff_size * 1024); 
+
 	if(sd < 0){
 		perror("sock_init failed");
 		exit(1);
 	}
+
 	struct sockaddr_in in_name;
 	struct hostent *hptr;
+	socklen_t addrlen = sizeof in_name;
 	memset((char *) &in_name, 0, sizeof in_name);
 	in_name.sin_family = AF_INET;
 	in_name.sin_port = htons(atoi(port));
 	hptr = gethostbyname(host);
 	memcpy((void *)&(in_name.sin_addr), hptr->h_addr_list[0], hptr->h_length);
-	char data[100];
-	strcpy(data, argv[3]);
-	if(sendto(sd, data, strlen(data), 0, (struct sockaddr *) &in_name, sizeof in_name) < 0){
-		perror("sendto failed");
-		close(sd);
-		exit(1);
+
+	/* Timeout */
+	struct timeval tv;
+	tv.tv_sec = 0;
+	tv.tv_usec = 50;
+	setsockopt(sd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof tv); 
+
+	/* Number of packet */
+	int num_of_packet = (reader.size + MAX_DATA_LENGTH - 1)/MAX_DATA_LENGTH;
+
+	int lar =0, lfs = 0;
+	frame *data = (frame *) malloc(buff_size * sizeof(frame));
+	uint8_t resp[7];
+	std::queue<pack_timeout> timeout;
+
+	printf("No Of frame :%d\n", num_of_packet);
+
+	/* Sliding Window */
+	while(lar < num_of_packet){
+		/* Proccess timeout */
+		// for(int i=lar+1;i<=lfs;++i){
+		// 	int idx = i % window_size;
+		// 	if(data[idx].timeout < time(NULL)){
+		// 		send_packet(i, data[idx].data, data[idx].length, sd, in_name);
+		// 		data[idx].timeout = time(NULL)+timeout_limit;
+		// 	}
+		// }
+		while(!timeout.empty() && timeout.front().timeout < time(NULL)){
+			pack_timeout tmp = timeout.front();
+			timeout.pop();
+			if(tmp.packet_no <= lar)
+				continue;
+			int idx = lfs % window_size;
+			sent_packet(lfs, data[idx].data, data[idx].length, sd, in_name);
+			tmp.timeout = time(NULL) + timeout_limit;
+			timeout.push(tmp);
+		}
+
+		/* Sending Data */
+		while(lfs < num_of_packet && lfs-lar < window_size){
+			int idx = ++lfs % window_size;
+			data[idx].length = std::min(reader.rem_size, (size_t) MAX_DATA_LENGTH);
+			reader.read_data(data[idx].data, data[idx].length);
+			sent_packet(lfs, data[idx].data, data[idx].length, sd, in_name);
+			timeout.push({lfs, time(NULL)+timeout_limit});
+		}
+
+		/* Get Response */
+		int tmp_len;
+		while((tmp_len = recvfrom(sd, resp, 10, 0, (struct sockaddr *) &in_name, &addrlen)) >= 0){
+			printf("Get response %d\n", tmp_len);
+			if(verify_response(resp, tmp_len)){
+				int seq_num = bytes_to_int(resp+1)-1;
+				if(resp[0] == NAK_NUMBER){
+
+				}
+				else{
+					printf("recieve ACK: %d : ", seq_num);
+					printf("%d %d %d %d %d %d\n", (int)resp[0], (int)resp[1], (int)resp[2], (int)resp[3], (int)resp[4], (int)resp[5]);
+					lar = std::max(lar, seq_num);	
+				}
+			}
+		}
 	}
+	sent_packet(++lfs, (uint8_t *)"", 0, sd, in_name);
+
 	close(sd);
 	return 0;
 }
